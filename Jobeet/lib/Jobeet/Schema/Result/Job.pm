214d15c74a52d32d6c340bfd6a3cae34e320297b
osawa-jun <osawa-jun@KYCNT1610-06.local>
2016-11-11 11:29:09 +0900

    day10 終了（確認ページのCSSに問題あり）

--- Jobeet/lib/Jobeet/Schema/Result/Job.pm
+++ Jobeet/lib/Jobeet/Schema/Result/Job.pm
@@ -5,12 +5,39 @@ use warnings;
 use parent 'Jobeet::Schema::ResultBase';
 use Jobeet::Schema::Types;
 use Jobeet::Models;
+use Digest::SHA1 qw/sha1_hex/;
+use Data::UUID;
+
 
 sub insert {
     my $self = shift;
 
     $self->expires_at( models('Schema')->now->add( days => models('conf')->{active_days} ) );
+
+    $self->token( sha1_hex(Data::UUID->new->create) );
+
     $self->next::method(@_);
+
+}
+
+sub is_expired {
+    my ($self) = @_;
+    $self->days_before_expired < 0;
+}
+
+sub days_before_expired {
+    my ($self) = @_;
+    ($self->expires_at - models('Schema')->now)->days;
+}
+
+sub expires_soon {
+    my ($self) = @_;
+    $self->days_before_expired < 5;
+}
+
+sub publish {
+    my ($self) = @_;
+    $self->update({ is_activated => 1 });
 }
 
 # ここにテーブル定義を書く
